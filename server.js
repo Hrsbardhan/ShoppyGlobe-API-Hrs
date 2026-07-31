db7ab7a99cedb9779cd57c1eb02549df4da1be0a
hrsbardhan <hrsbardhan@gmail.com>
2026-07-31 15:18:02 +0530

    Initial Express and MongoDB setup

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
+
+const PORT = process.env.PORT || 5000;
+
+(async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+})();
---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "ShoppyGlobe API is running"
+    });
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        success: true,
+        database: "Connected",
+        status: "OK"
+    });
+});
+
+module.exports = app;
