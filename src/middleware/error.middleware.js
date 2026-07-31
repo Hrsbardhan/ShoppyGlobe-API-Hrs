66b29f49c3147f02aa2c921f3c1894980a171473
hrsbardhan <hrsbardhan@gmail.com>
2026-07-31 17:36:17 +0530

    Add structured logging and improve production error monitoring

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,20 +1,45 @@
-const errorMiddleware = (err, req, res, next) => {
+const logger = require("../utils/logger");
 
-    console.error(err.stack);
+
+const errorMiddleware = (
+    err,
+    req,
+    res,
+    next
+) => {
+
+    logger.error({
+
+        message: err.message,
+
+        stack: err.stack,
+
+        path: req.originalUrl,
+
+        method: req.method
+
+    });
+
 
     res.status(
+
         err.statusCode || 500
+
     ).json({
 
         success: false,
 
         message:
+
             err.message ||
+
             "Internal Server Error",
 
         data: null
 
     });
+
 };
+
 
 module.exports = errorMiddleware;
